5d6dbbd779f4ba6fcc4eeaf88dc0144e2736cff4
Raphaël Mourtzanakis <raphael.mourtzanakis@glmdsio.fr>
2025-12-05 11:45:28 +0100

    Version 2.3

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,6 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/visiteur/initpwd', [VisiteurController::class, 'initPasswordAPI']);
+Route::post('/visiteur/auth', [VisiteurController::class, 'auth']);
+Route::post('/visiteur/logout', [VisiteurController::class, 'logout']);
+Route::post('/visiteur/unauthorized', [VisiteurController::class, 'unauthorized']);
