b12a4b6f8a27b9a84fdd38fe01bf8d4657457214
Raphaël Mourtzanakis <raphael.mourtzanakis@glmdsio.fr>
2025-12-12 11:47:24 +0100

    Version 6

--- app/Http/Controllers/FraisController.php
+++ app/Http/Controllers/FraisController.php
@@ -96,7 +96,9 @@ class FraisController extends Controller {
         try {
             $service = new FraisService();
             $unFrais = $service->getUnFrais($id);
-            return json_encode($unFrais);
+            return response()->json([
+                'data' => $unFrais,
+            ]);
         } catch(Exception $exception) {
             return response()->json(['error' => $exception->getMessage()], 500);
         }
@@ -111,7 +113,7 @@ class FraisController extends Controller {
             $unFrais->id_visiteur = $request->json('id_visiteur');
             $unFrais->nbjustificatifs = $request->json('nbjustificatifs');
             $unFrais->datemodification = today();
-            $unFrais->montantvalide = $request->json('datemodification');
+            $unFrais->montantvalide = $request->json('montantvalide');
             $service->saveUnFrais($unFrais);
 
             return response()->json([
@@ -127,7 +129,9 @@ class FraisController extends Controller {
         try {
             $service = new FraisService();
             $desFrais = $service->getListFrais($idVisiteur);
-            return json_encode($desFrais);
+            return response()->json([
+                'data' => $desFrais,
+            ]);
         } catch (Exception $exception) {
             return response()->json(['error' => $exception->getMessage()], 500);
         }
@@ -150,6 +154,35 @@ class FraisController extends Controller {
                 'old_data' => $ancienFrais,
                 'new_data' => $unFrais,
             ]);
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    function removeFraisAPI(Request $request) {
+        try {
+            $id = $request->json('id_frais');
+            $idVisiteur = $request->json('id_visiteur');
+            $service = new FraisService();
+            $unFrais = $service->getUnFrais($id);
+
+            if ($id && isset($unFrais) && $unFrais->id_visiteur == $idVisiteur) {
+                $service->deleteFrais($id, $idVisiteur);
+                return response()->json([
+                    'status' => 'Frais supprimé',
+                    'data' => $unFrais,
+                ]);
+            } else {
+                if ($id && isset($unFrais) && $unFrais->id_visiteur != $idVisiteur) {
+                    return response()->json([
+                        'error' => "Tu n'as pas accès à ce frais",
+                    ]);
+                } else {
+                    return response()->json([
+                        'error' => 'Frais inconnu',
+                    ]);
+                }
+            }
         } catch (Exception $exception) {
             return response()->json(['error' => $exception->getMessage()], 500);
         }
