dda2db9fe7e00bb492bbdfe8e0a06f2f35dcc4a8
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2026-03-13 15:45:04 +0100

    Version 10

---
+++ resources/views/listSpecialiteDePraticien.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container">
+        <h1>Liste des spécialités</h1>
+    </div>
+
+    @if (isset($specialites[0]["id_specialite"]))
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>Libellé</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($specialites as $ligne)
+            <tr>
+                <td>{{ $ligne->lib_specialite }}</td>
+                <td><a href="{{url("/Specialite/modifier/".$ligne->id_specialite)}}">Afficher</a></td>
+            </tr>
+        @endforeach
+        </tbody>
+        @else
+        <div class="container table-message">
+            <p>Aucune spécialité trouvée.</p>
+        </div>
+        @endif
+    </table>
+@endsection
